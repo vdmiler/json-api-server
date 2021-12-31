3c396c0985b7c73ccb95e90464f40c8097d963c9
vdmiler <vdmiler1985@gmail.com>
2021-12-31 14:08:31 +0300

    Change avatars in db.json

--- faker.js
+++ faker.js
@@ -5,7 +5,7 @@ const generateDataFile = (number) => {
    for (let i = 0; i < number; i++) {
       results.push({
          id: i,
-         photo: faker.image.cats(),
+         photo: `${faker.image.avatar()}?random=${Date.now()}`,
          first_name: faker.name.firstName(),
          birthday: faker.date.past(),
          desiredposition: faker.name.jobTitle(),
